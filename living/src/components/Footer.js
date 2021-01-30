c2fb3c1ec29a355dd236b601b9b7e88f5a0c4745
Drew R <drewr@Drews-MacBook-Pro.local>
2021-01-29 23:40:55 -0600

    save

---
+++ living/src/components/Footer.js
@@ -0,0 +1,27 @@
+import React, {Component} from 'react'
+
+class Footer extends Component {
+    render = () => {
+        return (
+            <div className='footer'>
+                <ul className='footlinks'>
+                    <li className='about'>About</li>
+                    <li className='terms'>Terms of Use</li>
+                    <li className='help'>Help</li>
+                    <li className='contact'>Contact Us</li>
+                    <li className='feedback'>Provide Feedback</li>
+                    <li className='social'>LinkedIn</li>
+                </ul>
+                <div className='userstory'>
+                    <p>Information about our User story, what makes use diffrent, our mission and a little bit about what inspired us.</p>
+                </div>
+            </div>
+
+            
+            
+
+        )
+    }
+}
+
+export default Footer
