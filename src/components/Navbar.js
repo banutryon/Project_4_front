e06f9d2e63459d166e05eab0068b414e14f3fdcb
Drew R <drewr@Drews-MacBook-Pro.local>
2021-01-30 20:30:48 -0600

    l

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,6 +12,74 @@ class Navbar extends Component {
                 <ul className='right-link'>
                     <li className='login'>Login</li>
                     <li className='signup'>Sign up</li>
+                    <li className='create'></li>
+                        <details>
+                            <summary>Create</summary>
+                            <label htmlFor="name">Name</label>
+                                 <input 
+                                 type="text" 
+                                 id="name"
+                                 onChange={this.props.handleChange}
+                                 value={this.props.state.name} /> 
+                                 <br />
+
+                                 <input 
+                                 type="text" 
+                                 id="street"
+                                 onChange={this.props.handleChange}
+                                 value={this.props.state.street} />
+                                 <br />
+
+                                  <input 
+                                 type="text" 
+                                 id="address"
+                                 onChange={this.props.handleChange}
+                                 value={this.props.state.address} />
+                                 <br />
+
+                                 <input 
+                                 type="text" 
+                                 id="state"
+                                 onChange={this.props.handleChange}
+                                 value={this.props.state.state} /> 
+                                 <br />
+
+                                  <input 
+                                 type="text" 
+                                 id="zipcode"
+                                 onChange={this.props.handleChange}
+                                 value={this.props.state.zipcode} />  
+                                 <br />
+
+                                 <input 
+                                 type="text" 
+                                 id="img1"
+                                 onChange={this.props.handleChange}
+                                 value={this.props.state.img2} />  
+                                 <br />
+
+                                 <input 
+                                 type="text" 
+                                 id="img2"
+                                 onChange={this.props.handleChange}
+                                 value={this.props.state.img2} />  
+                                 <br />
+
+                                 <input 
+                                 type="text" 
+                                 id="img3"
+                                 onChange={this.props.handleChange}
+                                 value={this.props.state.img3} />  
+                                 <br />
+
+                                 <input 
+                                 type="text" 
+                                 id="description"
+                                 onChange={this.props.handleChange}
+                                 value={this.props.state.description} />  
+                                 <br />
+
+                        </details>
                 </ul>
                 <div className='HomeImg'>
                     <img src="https://images.unsplash.com/photo-1508913950751-d1d139a29e68?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=751&q=80" alt='HomeImg'/>
