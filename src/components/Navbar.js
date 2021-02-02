dc42f3106ace920227d933593c337dd7033e9046
Ryan Fanelli <illenafnayr@gmail.com>
2021-02-02 18:09:01 -0500

    link to aboutus

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,7 +10,7 @@ export const Navbar = (props) => {
             <nav className='navbar'>
                 <ul className='ul'>
                     <li className='link aLeft1'><a href='#'>Buy</a></li>
-                    <li className='link aLeft2'><a href="#">About us</a></li>
+                    <li className='link aLeft2'><a href="/about">About us</a></li>
                     
                     
                     <a href="https://pure-stream-81491.herokuapp.com/"><img className='logo' src="https://lh3.googleusercontent.com/i6uumc_ebN47nE2tfJ8WEcOoi5fAwaSJrrBxlCojpKKiO2XVJhFlrglng_EaqsdTjKmWtbz0l6ibzYa8q7-EEXtOnfqTnOQqKEZqi8qTdzQfy7mhdhSLG3BkCLnQUbUI18Yqkp2sphc=w2400" alt="logo" /></a>
@@ -148,7 +148,7 @@ export const Navbar = (props) => {
 
                                  <div className="grid-item">
                                  <label htmlFor="name">Price</label>
-                                <br />
+                                <br />3000
                                  <input 
                                  type="text" 
                                  id="price"
@@ -184,7 +184,7 @@ export const Navbar = (props) => {
                                 state={props.state} />
                         </ProtectedRoute>
                     </li> 
-                    <li className='link aRight2'><a href='#'>Sign Up</a></li>
+                    {/* <li className='link aRight2'><a href='#'>Sign Up</a></li> */}
                 </ul>
             </nav>
         )
